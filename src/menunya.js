05001da4a59301b66bbcffcb11c75a15dbcd0cb2
rzmodderdevv <vektorbrazil@gmail.com>
2022-11-08 18:45:03 +0700

    rzmodderv1.1-md

---
+++ src/menunya.js
@@ -0,0 +1,37 @@
+const menunya = `
+
+╭━━•›ꪶ⸙ ━ ━ ━ ━ ꪶ⸙‹•━━╮
+┃╭┈─────────────⩵꙰ཱི࿐
+┃➡BOT: 𝑹𝒛 𝑴𝒐𝒅𝒅𝒆𝒓 - 𝑴𝑫
+┃➡MODE: Publik PLATFORM: android
+┃➡TYPE: Node.Js
+┃➡BAILEYS: Multi Device
+┃➡PREFIX: [.]
+┃➡DATABASE: 31 dari 228 ──➤ ↶↷
+╰━━•›ꪶ⸙ ━ ━ ━ ━ ꪶ⸙‹•━━͙✩̣̣̣̣
+ ▬▬▬▬▬ ♕♛♕ ▬▬▬▬▬
+╭━━•›〘 LIST MENU 〙
+➳ BUGMENU 
+➳ FUNMENU 
+➳ MAINMENU 
+➳ VOICEMENU 
+➳ GROUPMENU 
+➳ OWNERMENU 
+➳ ISLAMICMENU 
+➳ SEARCHMENU 
+➳ EPHOTOMENU 
+➳ RANDOMMENU 
+➳ TEKSPROMENU 
+➳ PRIMBONMENU 
+➳ CONVERTMENU 
+➳ WEBZONEMENU
+➳ DATABASEMENU 
+➳ PHOTOOXYMENU
+➳ DOWNLOADMENU
+➳ ANONYMOUSMENU
+╰━ ━ ━ ━ ━ ━ ━ ━ ━ ━•𝄞
+ ▬▬▬▬▬ ♕♛♕ ▬▬▬▬▬
+
+© 𝑹𝒁 𝑴𝑶𝑫𝑫𝑬𝑹 𝑫𝑬𝑽
+`
+exports.menunya = menunya
